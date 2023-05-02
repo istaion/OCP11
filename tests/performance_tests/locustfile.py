9dea2b17a673b427ed13c601d458361548d3e865
istaion <v.poutot@gmail.com>
2023-05-02 17:03:21 +0200

    Correctif d'une petite erreur locust

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -9,7 +9,7 @@ class ProjectPerfTest(HttpUser):
 
     @task
     def test_summary(self):
-        self.client.post('/showSummary', data={'email': 'john@simplylift.co'})
+        self.client.post('/show_summary', data={'email': 'john@simplylift.co'})
 
     @task
     def test_book(self):
@@ -17,12 +17,12 @@ class ProjectPerfTest(HttpUser):
 
     @task
     def test_purchase(self):
-        self.client.post('/purchasePlaces', data={'club': 'Simply Lift',
-                                                  'competition': 'Spring Festival', 'places': '1'})
+        self.client.post('/purchase_places', data={'club': 'Simply Lift',
+                                                   'competition': 'Spring Festival', 'places': '1'})
 
     @task
     def test_rankings(self):
-        self.client.get('/showPoints')
+        self.client.get('/show_points')
 
     @task
     def test_logout(self):
